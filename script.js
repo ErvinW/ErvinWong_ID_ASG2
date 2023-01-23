ffc54185d1e4e8eaf711545fe64e5b7a851bf5d0
ErvinW <116048866+ErvinW@users.noreply.github.com>
2023-01-23 20:36:17 +0800

    Add js script to check user login input against User object in RestDB
    Js script added checks the user's login input against the RestDB database. If user input matches to an object in the database, they are then redirected to page2.html

---
+++ script.js
@@ -0,0 +1,100 @@
+$(document).ready(function(){
+    const User_list = [];
+
+
+    function User(User_Email, User_password){
+        this.User_Email = User_Email,
+        this.User_password = User_password
+
+        
+    }
+
+    
+
+
+        //Get values when user clicks on login 
+        // let jsondata = {
+        //     "User_Email" : UserEmail,
+        //     "User_password" : Password
+        // };
+
+
+        //From RestDB
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://idasg2-d416.restdb.io/rest/useraccount",
+        "method": "GET",
+        "headers": {
+          "content-type": "application/json",
+          "x-apikey": "63ce3dc1969f06502871b0f1",
+          "cache-control": "no-cache"
+        }
+      }
+      
+      $.ajax(settings).done(function (response) {
+        //console.log(response);
+        for (let i = 0; i < response.length; i++){
+            User_list.push( new User(response[i].User_Email, response[i].User_password));
+            console.log(User_list[i])
+        }
+      });
+
+
+
+      //Form Listener
+    $("#submit").on("click", function(e){
+        e.preventDefault();
+
+
+        let User_Email = $("#email").val();
+        let User_password = $("#password").val();
+
+        for (let i = 0; i < User_list.length; i++){
+            if (User_Email == User_list[i].User_Email && User_password == User_list[i].User_password){
+                window.location.assign("page2.html"); 
+            }
+        }
+    })
+
+    
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var settings = {
+//     "async": true,
+//     "crossDomain": true,
+//     "url": "https://idasg2-d416.restdb.io/rest/useraccount",
+//     "method": "GET",
+//     "headers": {
+//       "content-type": "application/json",
+//       "x-apikey": "63ce3dc1969f06502871b0f1",
+//       "cache-control": "no-cache"
+//     }
+//   }
+  
+//   $.ajax(settings).done(function (response) {
+//     console.log(response);
+//   });
+
+  
