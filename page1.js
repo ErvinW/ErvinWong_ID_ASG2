deee85cf23005d93fc6a436a517ea934c74a3581
Seafone <s10244252@connect.np.edu.sg>
2023-02-01 23:29:07 +0800

    doing shopping cart functions

--- page1.js
+++ page1.js
@@ -1,5 +1,10 @@
 let products = document.getElementById("products")
 
+let shopitemdata = [
+    ];
+
+let basket = [];
+
 let productdata = [
     {
         id: "Item-1",
@@ -35,7 +40,8 @@ let productdata = [
 let printproducts =()=>{
     return (products.innerHTML = productdata.map((i)=>{
         let {id, ProductName, Price, ProdDesc, img} = i; /* De-structuring */
-        return  `<div id = product-id-${id} class="Headphones">    
+        return  `
+        <div id = product-id-${id} class="Headphones">    
         <img width = "300" src= ${img} alt="">
         <div class="productdeets">
             <h2>${ProductName}</h2>
@@ -43,9 +49,9 @@ let printproducts =()=>{
             <div class="price-amt">
                 <h2>$${Price}</h2>
                 <div class="button">
-                    <i class="bi bi-plus-lg"></i>
+                    <i onclick="decrement(${id})" class="bi bi-plus-lg"></i>
                     <div id=${id} class="Quantity">0</div>
-                    <i class="bi bi-dash-lg"></i>
+                    <i onclick="increment(${id})" class="bi bi-dash-lg"></i>
                 </div>
             </div>
         </div>
@@ -54,5 +60,43 @@ let printproducts =()=>{
 };
 
 printproducts();
+
+let increment = (id) =>{
+    let selectedItem = id;
+    let search = basket.find((x)=> x.id === selectedItem.id);
+
+    if(search === undefined){
+        basket.push({
+            id: selectedItem.id,
+            item: 1,
+        });
+    }
+    else{
+        search.id += 1;
+    }
+
+    //console.log(basket);
+    update(selectedItem.id);
+};
+
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x)=> x.id === selectedItem.id);
+
+    if(search.item === 0)return;
+
+    else{
+        search.id -= 1;
+    }
+
+    //console.log(basket);
+    update();
+};
+
+let update = (id) => {
+    let search = basket.find((x)=>x.id === id);
+    console.log(search);
+    document.getElementById(id).innerHTML = search.item;
+};
 
 
