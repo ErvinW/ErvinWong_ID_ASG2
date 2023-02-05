363523dd612633c2baef53be3e2c0f709175e5a1
ErvinW <116048866+ErvinW@users.noreply.github.com>
2023-02-05 16:38:59 +0800

    Add delete account function(Not fully working)

--- products.js
+++ products.js
@@ -1,2 +1,3 @@
-const obj = JSON.parse(localStorage.getItem("User"));
+var obj = JSON.parse(localStorage.getItem("User"));
+console.log(obj);
 document.getElementById("Welc").innerHTML = "Welcome! " + obj.Username;
