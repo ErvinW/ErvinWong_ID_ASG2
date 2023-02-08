7e5f46457499b688adf484159fd11e89001fcd7b
Seafone <s10244252@connect.np.edu.sg>
2023-02-08 22:51:06 +0800

    CSS

--- Page1/page1.js
+++ Page1/page1.js
@@ -9,14 +9,12 @@ let productdata = [
         id: "Item-1",
         ProductName: "Sony WH-1000XM5",
         Price: 499.90,
-        ProdDesc: "Product desc of the XM5s",
         img: "/Images/XM5.jpg",
     },
     {
         id: "Item-2",
         ProductName: "Sennheiser Momentum 4",
         Price: 499.00,
-        ProdDesc: "Product desc of the Sennheisers",
         img: "/Images/SennM4.jpg",
 
     },
@@ -24,28 +22,24 @@ let productdata = [
         id: "Item-3",
         ProductName: "Sony WH-1000XM4",
         Price: 399.00,
-        ProdDesc: "Product desc of the XM4s",
         img: "/Images/XM4.jpg",
     },
     {
         id: "Item-4",
         ProductName: "Anker Soundcore Space 45",
         Price: 209.95,
-        ProdDesc: "Product desc of the 45s",
         img: "/Images/SoundCore45.jpg",
         
     }, {
         id: "Item-5",
         ProductName: "Moondrop Aria",
         Price: 112.41,
-        ProdDesc: "Product desc of the Arias",
         img: "/Images/Arias.jpg",
     },
     {
         id: "Item-6",
         ProductName: "Moondrop Chu",
         Price: 499.00,
-        ProdDesc: "Product desc of the Chus",
         img: "/Images/Chu.jpg",
 
     },
@@ -53,14 +47,12 @@ let productdata = [
         id: "Item-7",
         ProductName: "7Hz Salnotes Dioko",
         Price: 149.00,
-        ProdDesc: "Product desc of the Diokos",
         img: "/Images/Dioko.jpg",
     },
     {
         id: "Item-8",
-        ProductName: "Sennheiser Momentum True wireless 3",
+        ProductName: "Sennheiser Momentum True Wireless 3",
         Price: 322.89,
-        ProdDesc: "Product desc of the SennM3s",
         img: "/Images/SennM3.jpg",
         
     },
@@ -68,14 +60,12 @@ let productdata = [
         id: "Item-9",
         ProductName: "Beyerdyanmic DT990 Pro",
         Price: 228.00,
-        ProdDesc: "Product desc of the BD 990s",
         img: "/Images/Beyer.jpg"
     },
     {
         id: "Item-10",
         ProductName: "Razer Hammerhead 2021 TWS",
         Price: 169.90,
-        ProdDesc: "Prodcut desc of the hammerhead 2021",
         img: "/Images/Hammerhead.jpg"
     }
     ]
@@ -89,7 +79,6 @@ let productdata = [
         <img width = 300px height = 185px src= ${productdata[i].img} alt=""></img>
         <div class="productdeets">
             <h2 id="ProductName">${productdata[i].ProductName}</h2>
-            <p>${productdata[i].ProdDesc}</p>
             <div class="price-amt">
                 <h2 id="price-amt">$${productdata[i].Price}</h2>
             </div>
