c3502e5faa074440a4ed4b3b50929540d9688a66
ErvinW <116048866+ErvinW@users.noreply.github.com>
2023-02-09 22:44:37 +0800

    Minor changes to order function

--- Index/script.js
+++ Index/script.js
@@ -83,20 +83,5 @@ $(document).ready(function(){
 
 
 
-// var settings = {
-//     "async": true,
-//     "crossDomain": true,
-//     "url": "https://idasg2-d416.restdb.io/rest/useraccount",
-//     "method": "GET",
-//     "headers": {
-//       "content-type": "application/json",
-//       "x-apikey": "63ce3dc1969f06502871b0f1",
-//       "cache-control": "no-cache"
-//     }
-//   }
-  
-//   $.ajax(settings).done(function (response) {
-//     console.log(response);
-//   });
 
   
